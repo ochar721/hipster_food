15f0e32f395f333380e0b753043f289bcc63e665
Olivia Charles <ocharles92@gmail.com>
2021-01-12 13:29:06 -0700

    Created test for food truck exists and has attributes

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -1,29 +1,37 @@
-pry(main)> food_truck = FoodTruck.new("Rocky Mountain Pies")
-#=> #<FoodTruck:0x00007f85683152f0...>
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/food_truck'
 
-pry(main)> food_truck.name
-#=> "Rocky Mountain Pies"
 
-pry(main)> food_truck.inventory
-#=> {}
+class FoodTruckTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    assert_instance_of FoodTruck, food_truck
+    assert_equal "Rocky Mountain Pies", food_truck.name
+  end
+end
 
-pry(main)> food_truck.check_stock(item1)
-#=> 0
 
-pry(main)> food_truck.stock(item1, 30)
-
-pry(main)> food_truck.inventory
-#=> {#<Item:0x007f9c56740d48...> => 30}
-
-pry(main)> food_truck.check_stock(item1)
-#=> 30
-
-pry(main)> food_truck.stock(item1, 25)
-
-pry(main)> food_truck.check_stock(item1)
-#=> 55
-
-pry(main)> food_truck.stock(item2, 12)
-
-pry(main)> food_truck.inventory
-#=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
+#  food_truck.inventory
+# #=> {}
+#
+#  food_truck.check_stock(item1)
+# #=> 0
+#
+#  food_truck.stock(item1, 30)
+#
+#  food_truck.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 30}
+#
+#  food_truck.check_stock(item1)
+# #=> 30
+#
+#  food_truck.stock(item1, 25)
+#
+#  food_truck.check_stock(item1)
+# #=> 55
+#
+# pry(main)> food_truck.stock(item2, 12)
+#
+# pry(main)> food_truck.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
